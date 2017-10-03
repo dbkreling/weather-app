5af36e5abff348e34672f2b00d2fc74632e6a69d
Daniel Kreling <dbkreling@gmail.com>
2017-10-03 17:45:33 -0300

    app.js: handle server and address errors
    This commit introduces handlers for server errors as well as invalid
    addresses.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- app.js
+++ app.js
@@ -13,15 +13,19 @@ const argv = yargs
     .help('help', 'h')
     .argv;
 
-console.log(argv);
-
 var encodedAddress = encodeURIComponent(argv.address);
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK'){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
