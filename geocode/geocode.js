bbb1d1ffb407c3e208da9cbd564e858d5f7f751f
Daniel Kreling <dbkreling@gmail.com>
2017-10-03 23:13:51 -0300

    app.js: add a callback function to geolocation
    Make geolocation a callback function
    
    Ref.: Lecture 33
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
     var encodedAddress = encodeURIComponent(address);
     request({
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
         json: true
     }, (error, response, body) => {
-        if (error) {
-            console.log('Unable to connect to Google servers.');
+        if (error) { 
+            calback('Unable to connect to Google servers.');
         } else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find that address.');
+            callback('Unable to find that address.');
         } else if (body.status === 'OK'){
-            console.log(`Address: ${body.results[0].formatted_address}`);
-            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            })
         }
     });
 };
