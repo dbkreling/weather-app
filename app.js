144faa18574e9fb1f04c2a26313aa71b3e34ab0c
Daniel Kreling <dbkreling@gmail.com>
2017-10-04 18:12:51 -0300

    app.js: fetch weather statically
    This is a preparation for future implementation to fetch weather dynamically.
    So far, this patch is introducing a working draft which fetch temperature and
    response.statusCode and also handles errors.
    
    Ref.: Lecture 34
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             description: 'Address to fetch the weather for',
+//             string: true  // Reinforce we get data in the string format
+//         }
+//     })
+//     .help('help', 'h')
+//     .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+const request = require('request');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            description: 'Address to fetch the weather for',
-            string: true  // Reinforce we get data in the string format
-        }
-    })
-    .help('help', 'h')
-    .argv;
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+request({
+    url: `https://api.darksky.net/forecast/914e2c5300e562b7cc3c33b135beb578/-22.9098833,-47.0625812`,
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+        console.log(response.statusCode);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('Unable to fetch weather');
     }
 });
