3dea5b6ffda35dda034b29b70ddb9a0efaf51b22
Daniel Kreling <dbkreling@gmail.com>
2017-10-05 17:39:50 -0300

    app.js: chaining callbacks together
    This commit links from weather.getWeather() and geocode.geocodeAddress, making
    the app outputs temperatures based on ZIP code and location name inputs, making it
    more real-life like.
    
    This commit also abstracts the darksky and Google's geocode API keys. They are stored
    in a local folder that will need to be recreated in the client's system if ever there
    is one.
    
    NOTE: The app will not work beyond this point without the API keys in place. So the user
    must consider that and set it up accordingly withh his own keys.
    
    Ref.: Lecture 35
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: 'address',
-//             description: 'Address to fetch the weather for',
-//             string: true  // Reinforce we get data in the string format
-//         }
-//     })
-//     .help('help', 'h')
-//     .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    url: `https://api.darksky.net/forecast/914e2c5300e562b7cc3c33b135beb578/-22.9098833,-47.0625812`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature);
-        console.log(response.statusCode);
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            description: 'Address to fetch the weather for',
+            string: true  // Reinforce we get data in the string format
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unable to fetch weather');
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(results.address);
+                console.log(`It is currently ${weatherResults.temperature}` +
+                    ` in ${results.cityName}, but it feels like ${weatherResults.apparentTemperature}.`);
+            }
+        });
     }
 });
---
+++ keys.js
@@ -0,0 +1,7 @@
+const googleGeocode='insert the googleGeocode API key here';
+const darksky='insert the darksky API key here.';
+
+module.exports = {
+    googleGeocode,
+    darksky
+};
