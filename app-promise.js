74dc419266f4aecc113e411283087a942069ca4c
Daniel Kreling <dbkreling@gmail.com>
2017-10-08 22:17:44 -0300

    app-promise: implement code to use axios
    Axios is a library that supports promises when making requests to APIs
    and this commit implements code to make use of that.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

---
+++ app-promise.js
@@ -0,0 +1,43 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const keys = require('./keys');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            description: 'Address to fetch the weather for',
+            string: true  // Reinforce we get data in the string format
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?${keys.googleGeocode}&address=${encodedAddress}`
+
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address.');
+    }
+
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/${keys.darksky}/${lat},${lng}`;
+
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It is ${temperature}. It feels like ${apparentTemperature}`);
+}).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers.');
+    } else {
+        console.log(e.message);
+    }
+});
